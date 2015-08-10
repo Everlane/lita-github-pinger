9716c46007f806d12b8c2b03cfa9f4a83d3e0ffe
Taylor Lapeyre <taylorlapeyre@gmail.com>
2015-08-10 11:59:18 -0700

    better pings

--- lib/lita/handlers/github_pinger.rb
+++ lib/lita/handlers/github_pinger.rb
@@ -2,21 +2,32 @@ module Lita
   module Handlers
     class GithubPinger < Handler
 
+      GITHUB_PR_COMMENT_REGEX = /\[Everlane\/.*\] New comment on pull request #(\d+): (.+) \(assigned to (\w+)\)\nComment by (\w+)\n(.*)/
+
       config :engineers, type: Array, required: true
 
-      route(/@(\w*)/, :detect_comment, command: false)
+      route(GITHUB_PR_COMMENT_REGEX, :detect_comment, command: false)
 
       def detect_comment(message)
-        return unless message.user.metadata["name"] == "" # Integrations don't have names
-        mentioned_username = message.matches[0][0]
+        pr_id,
+        pr_title,
+        assigned_person,
+        comment_author,
+        comment_text = message.matches[0]
 
-        # side effects intentional
-        found = config.engineers.any? do |eng|
-          message.reply("@" + eng[:slack]) if eng[:github] == mentioned_username
-        end
+        # return unless message.user.metadata["name"] == "" # Integrations don't have names
+        return unless comment_text.include?("@")
 
-        unless found
-          message.reply("Could not find a slack username for #{pr_owner}. Please configure everbot to include this username.")
+        mentioned_username = comment_text.split("@")[1].split(" ").first
+
+        config.engineers.each do |engineer|
+          if engineer[:github] == mentioned_username
+            content = %Q{#{engineer[:slack]}, you were mentioned by #{comment_author}:
+  http://github.com/everlane/everlane.com/pulls/#{pr_id}
+  > #{comment_text}
+}
+            message.reply(content)
+          end
         end
       end
     end
--- spec/lita/handlers/github_pinger_spec.rb
+++ spec/lita/handlers/github_pinger_spec.rb
@@ -43,7 +43,14 @@ describe Lita::Handlers::GithubPinger, lita_handler: true do
   end
 
   it "will respond" do
-    send_message("comments up @taylorlapeyre)")
-    expect(replies.count).to eq 0
+    send_message(%q{
+[Everlane/everlane.com] New comment on pull request #1137: More accurate location data and (by extension) working weather for factories (assigned to taylorlapeyre)
+Comment by thenanyu
+@taylorlapeyre hai
+})
+    expect(replies.last).to eq %q{taylor, you were mentioned by thenanyu:
+  http://github.com/everlane/everlane.com/pulls/1137
+  > @taylorlapeyre hai
+}
   end
 end
