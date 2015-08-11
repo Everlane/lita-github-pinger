8fdeab23ff3ab3d52b540b8b756f32adbe684a30
Taylor Lapeyre <taylorlapeyre@gmail.com>
2015-08-11 14:13:40 -0700

    refactoring some

--- lib/lita/handlers/github_pinger.rb
+++ lib/lita/handlers/github_pinger.rb
@@ -7,75 +7,69 @@ module Lita
       http.post("/ghping", :ghping)
 
       def ghping(request, response)
-
         puts "########## New GH PR Event! ##########"
-        body = MultiJson.load(request.body)
 
+        body = MultiJson.load(request.body)
         if body["comment"]
           puts "Detected a comment. Extracting data... "
 
-          thing     = body["pull_request"] || body["issue"]
-          pr_url    = thing["html_url"]
-          comment   = body["comment"]["body"]
-          commenter = body["comment"]["user"]["login"]
-          pr_owner  = thing["user"]["login"]
+          comment_url = body["comment"]["html_url"]
+          comment     = body["comment"]["body"]
+          context     = body["pull_request"] || body["issue"]
 
-          puts "Found PR #{pr_url}"
+          commenter = find_engineer(github: body["comment"]["user"]["login"])
+          pr_owner  = find_engineer(github: context["user"]["login"])
+
+          puts "Reacting to PR comment #{comment_url}"
           puts "Found commenter #{commenter}"
           puts "Found pr owner #{pr_owner}"
 
-          usernames_to_ping = []
+          engineers_to_ping = []
           # automatically include the creator of the PR, unless he's
           # commenting on his own PR
-          if commenter != pr_owner
-            puts "Commenter is not the pr owner. Adding to list of usernames to ping."
-            usernames_to_ping << pr_owner
-          end
 
-          puts "So far, github usernames to ping: #{usernames_to_ping}"
+          if commenter != pr_owner && ["all_discussion", nil].include?(pr_owner[:frequency])
+            puts "PR owner was not the commenter, and has a :frequency of 'all_discussion' or nil"
+            puts "Therefore, adding the PR owner to list of engineers to ping."
+            engineers_to_ping << pr_owner
+          end
 
           # Is anyone mentioned in this comment?
           if comment.include?("@")
             puts "Found @mentions in the body of the comment! Extracting usernames... "
 
-            # get each @mentioned username in the comment
+            # get each @mentioned engineer in the comment
             mentions = comment.split("@")[1..-1].map { |snip| snip.split(" ").first }
-            puts "Done. (Got #{mentions})"
+
+            puts "Done. Got #{mentions}"
+            puts "Converting usernames to engineers..."
+
+            mentioned_engineers = mentions.map { |username| find_engineer(github: username) }
+
+            puts "Done. Got #{mentioned_engineers}"
 
             # add them to the list of usernames to ping
-            usernames_to_ping = usernames_to_ping.concat(mentions).uniq
+            engineers_to_ping = engineers_to_ping.concat(mentioned_engineers).uniq.compact
           end
 
-          puts "New list of github usernames to ping: #{usernames_to_ping}."
-          puts "Converting github usernames to slack usernames... "
-
-          # slackify all of the users
-          usernames_to_ping.map! { |user| github_to_slack_username(user) }
-
-          puts "Done. (Got #{usernames_to_ping})"
-
+          puts "New list of engineers to ping: #{engineers_to_ping}."
           puts "Starting pinging process for each engineer..."
-          usernames_to_ping.compact.each do |user|
+          engineers_to_ping.each do |engineer|
+            puts "looking at #{engineer}'s preferences..'"
+            next if engineer[:frequency] == "off"
 
-            pref = find_engineer(slack: user)[:preference]
-
-            puts "Found preference #{pref.inspect} for user #{user}"
-
-            case pref
-            when "off"
-              puts "Preference was 'off', so doing nothing."
+            case engineer[:ping_location]
             when "dm", nil
               puts "Preference was either 'dm' or nil, so sending DM."
-              private_message  = "New PR comment from @#{commenter}:\n"
-              private_message += "#{pr_url}\n#{comment}"
-              send_dm(user, private_message)
+              private_message  = "New PR comment from @#{commenter[:slack]}:\n"
+              private_message += "#{comment_url}\n#{comment}"
+              send_dm(engineer[:slack], private_message)
             when "eng-pr", "eng_pr"
               puts "Preference was either 'eng-pr' or 'eng_pr', so alerting #eng-pr."
-              public_message  = "@#{user}, new PR mention: "
-              public_message += "#{pr_url}\n#{comment}" if user == usernames_to_ping.last
+              public_message  = "@#{engineer[:slack]}, new PR mention: "
+              public_message += "#{comment_url}\n#{comment}"
               alert_eng_pr(public_message)
             end
-
           end
 
           puts "GitHub Hook successfully processed."
@@ -100,11 +94,6 @@ module Lita
             eng[:github] == github
           end
         end.first
-      end
-
-      def github_to_slack_username(github_username)
-        engineer = find_engineer(github: github_username)
-        engineer[:slack] if engineer
       end
 
       def send_dm(username, content)
