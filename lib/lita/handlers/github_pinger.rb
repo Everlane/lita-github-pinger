0fef65daee92c0252eb53081d3031e3f56f0eb35
Taylor Lapeyre <taylorlapeyre@gmail.com>
2015-08-10 17:45:11 -0700

    add @mention detection

--- lib/lita/handlers/github_pinger.rb
+++ lib/lita/handlers/github_pinger.rb
@@ -12,31 +12,33 @@ module Lita
         body = MultiJson.load(request.body)
 
         if body["comment"]
-
-          thing = body["pull_request"] || body["issue"]
-
-          pr_url = thing["html_url"]
-          pr_owner = thing["user"]["login"]
+          thing     = body["pull_request"] || body["issue"]
+          pr_url    = thing["html_url"]
+          comment   = body["comment"]["body"]
           commenter = body["comment"]["user"]["login"]
-          comment = body["comment"]["body"].split("\n").join("\n >")
 
-          pr_owner = config.engineers.select do |eng|
-            eng[:github] == pr_owner
-          end.first[:slack]
+          # automatically include the creator of the PR
+          usernames_to_ping = [thing["user"]["login"]]
 
-          commenter = config.engineers.select do |eng|
-            eng[:github] == commenter
-          end.first[:slack]
+          # Is anyone mentioned in this comment?
+          if comment.include?("@")
+            # get each @mentioned username in the comment
+            mentions = comment.split("@")[1..-1].map { |snip| snip.split(" ").first }
+
+            # add them to the list of usernames to ping
+            usernames_to_ping = usernames_to_ping.concat(mentions).uniq
+
+            # slackify all of the users
+            usernames_to_ping.map! { |user| github_username_to_slack_username(user) }
+          end
 
           message  = "New PR comment from #{commenter}:\n"
-          message += "#{pr_url}\n> #{comment}"
+          message += "#{pr_url}\n#{comment}"
 
-          puts "Got a comment on something, sending message to #{pr_owner}"
+          puts "Got a comment on something, sending messages to #{usernames_to_ping}"
 
-          send_dm(pr_owner, message)
-
+          usernames_to_ping.each { |user| send_dm(user, message) }
         end
-
 
         response
       end
@@ -45,6 +47,12 @@ module Lita
         room = Lita::Room.fuzzy_find("eng")
         source = Lita::Source.new(room: room)
         robot.send_message(source, message)
+      end
+
+      def github_username_to_slack_username(github_username)
+        config.engineers.select do |eng|
+          eng[:github] == github_username
+        end.first[:slack]
       end
 
       def send_dm(username, content)
