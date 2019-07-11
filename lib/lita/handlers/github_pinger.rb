86af9ae76133526a8a5834b119563e27d1342283
Marshall Cheng <marshallycheng@gmail.com>
2019-07-11 14:06:53 -0700

    Revert "Rip out all DMs except deployment notifications"
    This reverts commit e155869e2b0addeb23b04b8dd6dac0023979fbb9.

--- lib/lita/handlers/github_pinger.rb
+++ lib/lita/handlers/github_pinger.rb
@@ -60,6 +60,220 @@ module Lita
         if body['deployment_status']
           act_on_deployment_status(body, response)
         end
+
+        if body["comment"] && body["action"] == "created"
+          act_on_comment(body, response)
+        end
+
+        if body["action"] && body["action"] == "assigned"
+          act_on_assign(body, response)
+        end
+
+        if body["action"] && body["action"] == "review_requested"
+          act_on_review_requested(body, response)
+        end
+
+        if body["action"] && body["action"] == "labeled"
+          act_on_pr_labeled(body, response)
+        end
+
+        if body["state"] && body["state"] == "success"
+          act_on_build_success(body, response)
+        end
+
+        if body["state"] && body["state"] == "failure"
+          act_on_build_failure(body, response)
+        end
+      end
+
+      def act_on_build_failure(body, response)
+        commit_url = body["commit"]["html_url"]
+        committer = find_engineer(github: body["commit"]["committer"]["login"])
+
+        puts "Detected a status failure for commit #{body["sha"]}"
+        message = ":x: Your commit failed CI."
+        message += "\n#{commit_url}"
+
+        if committer
+          frequency = if committer[:travis_preferences]
+            committer[:travis_preferences][:frequency]
+          else
+            committer[:status_preferences][:frequency]
+          end
+
+          return if ["off", "only_passes"].include?(frequency)
+
+          send_dm(committer[:usernames][:slack], message)
+        else
+          puts "Could not find configuration for GitHub username " + body["commit"]["committer"]["login"]
+        end
+
+        response
+      end
+
+      def act_on_build_success(body, response)
+        commit_url = body["commit"]["html_url"]
+        committer = find_engineer(github: body["commit"]["committer"]["login"])
+
+        puts "Detected a status success for commit #{body["sha"]}"
+        message = ":white_check_mark: Your commit has passed CI."
+        message += "\n#{commit_url}"
+
+        if committer
+          frequency = if committer[:travis_preferences]
+            committer[:travis_preferences][:frequency]
+          else
+            committer[:status_preferences][:frequency]
+          end
+
+          return if ["off", "only_failures"].include?(frequency)
+
+          send_dm(committer[:usernames][:slack], message)
+        else
+          puts "Could not find configuration for GitHub username " + body["commit"]["committer"]["login"]
+        end
+
+        response
+      end
+
+      def act_on_assign(body, response)
+        type = detect_type(body)
+
+        if type.nil?
+          puts "Neither pull request or issue detected, exiting..."
+          return
+        end
+
+        puts "Detected that someone got assigned to a #{type.tr('_', ' ')}."
+
+        assignee_login = body[type]["assignee"]["login"]
+        assignee = find_engineer(github: assignee_login)
+
+        puts "Looking up preferences..."
+        should_notify = assignee[:github_preferences][:notify_about_assignment]
+
+        if !should_notify
+          puts "will not notify, preference for :github_preferences[:notify_about_assignment] is not true"
+          return response
+        end
+
+        puts "#{assignee} determined as the assignee."
+
+        url = body[type]["html_url"]
+
+        message = "*Heads up!* You've been assigned to a #{type.tr('_', ' ')}:\n#{url}"
+
+        puts "Sending DM to #{assignee}..."
+        send_dm(assignee[:usernames][:slack], message)
+
+        response
+      end
+
+      def act_on_review_requested(body, response)
+        puts "Detected a review request."
+
+        puts "looking at previously notified reviewers for this PR"
+
+        pr = body["pull_request"]
+
+        url = pr["html_url"]
+
+        notified_engineers = redis.get(REVIEW_REDIS_KEY + ":" + url)
+
+        notified_engineers = if notified_engineers
+          JSON.parse(notified_engineers)
+        else
+          []
+        end
+
+        pr["requested_reviewers"].each do |reviewer|
+          engineer = find_engineer(github: reviewer["login"])
+
+          if !engineer
+            puts "Could not find engineer #{reviewer["login"]}"
+            next
+          end
+
+          if notified_engineers.include?(reviewer["login"])
+            puts "#{reviewer["login"]} has already been notified to review PR, skipping..."
+            next
+          end
+
+          puts "#{engineer} determined as a reviewer."
+
+          puts "Looking up preferences..."
+          should_notify = engineer[:github_preferences][:notify_about_review_requests]
+
+          if !should_notify
+            puts "will not notify, preference for :github_preferences[:notify_about_review_requests] is not true"
+          else
+            message = "You've been asked to review a pull request:\n#{url}"
+            send_dm(engineer[:usernames][:slack], message)
+            notified_engineers.push(reviewer["login"])
+          end
+        end
+
+        redis.set(REVIEW_REDIS_KEY + ":" + url, notified_engineers.to_json)
+
+        response
+      end
+
+      def act_on_pr_labeled(body, response)
+        type = detect_type(body)
+        puts "Detected that someone labeled a #{type.tr('_', ' ')}."
+
+        if type.nil?
+          puts "Neither pull request or issue detected, exiting..."
+          return
+        end
+
+        if body["pull_request"]["labels"].none? { |label| label["name"].downcase.include?('review') }
+          puts "Labels do not include a review label, exiting..."
+          return
+        end
+
+        if config.enable_round_robin
+          puts "round robin is enabled, selecting the next engineer.."
+
+          chosen_reviewer = get_next_round_robin_reviewer
+
+          pr_owner = find_engineer(github: body["pull_request"]["user"]["login"])
+          pr_owner = pr_owner[:usernames][:slack] unless pr_owner.nil?
+
+          if chosen_reviewer === pr_owner
+            update_next_round_robin_reviewer
+            chosen_reviewer = get_next_round_robin_reviewer
+          end
+
+          puts "#{chosen_reviewer} determined as the reviewer."
+
+          url = body[type]["html_url"]
+
+          message_for_reviewer = <<~eos
+            You’re next in line to look at a PR: #{body["pull_request"]["html_url"]}
+            There’s no obligation to submit a review, but take a look and familiarize yourself with the code as time allows.
+            If the PR looks particularly interesting or well-authored, nominate it as a PR of the Week.
+          eos
+          message_for_owner = <<~eos
+            #{chosen_reviewer} has been selected via round-robin to examine #{body["pull_request"]["html_url"]}.
+            Round-robin assignment is not an obligation to submit a review. Seek reviewers as appropriate.
+          eos
+
+          puts "Sending DM to #{chosen_reviewer}..."
+          send_dm(chosen_reviewer, message_for_reviewer)
+
+          if pr_owner
+            puts "Notifying #{pr_owner} of assignment."
+            send_dm(pr_owner, message_for_owner)
+          else
+            puts "Couldn't find a config for pr owner #{body["pull_request"]["user"]["login"]}. Make sure they are in the lita config!"
+            puts "Skipping notifying PR owner of RR assignment."
+          end
+
+          update_next_round_robin_reviewer
+          
+          response
+        end
       end
 
       def act_on_deployment_status(body, response)
@@ -96,6 +310,80 @@ module Lita
         end
       end
 
+      def act_on_comment(body, response)
+        puts "Detected a comment. Extracting data... "
+
+        comment_url = body["comment"]["html_url"]
+        comment     = body["comment"]["body"]
+        context     = body["pull_request"] || body["issue"]
+
+        commenter = find_engineer(github: body["comment"]["user"]["login"])
+        pr_owner  = find_engineer(github: context["user"]["login"])
+        lita_commenter = Lita::User.fuzzy_find(commenter[:usernames][:slack])
+
+        puts "Reacting to PR comment #{comment_url}"
+        puts "Found commenter #{commenter}"
+        puts "Found pr owner #{pr_owner}"
+
+        # Sanity Checks - might be a new engineer around that hasn't set up
+        # their config.
+
+        engineers_to_ping = []
+        # automatically include the creator of the PR, unless he's
+        # commenting on his own PR
+
+        if commenter != pr_owner && ["all_discussion", nil].include?(pr_owner[:github_preferences][:frequency])
+          puts "PR owner was not the commenter, and has a :frequency of 'all_discussion' or nil"
+          puts "Therefore, adding the PR owner to list of engineers to ping."
+          engineers_to_ping << pr_owner
+        end
+
+        # Is anyone mentioned in this comment?
+        if comment.include?("@")
+          puts "Found @mentions in the body of the comment! Extracting usernames... "
+
+          # get each @mentioned engineer in the comment
+          mentions = comment
+            .split("@")[1..-1] # "a @b @c d" => ["b ", "c d"]
+            .map { |snip| snip.split(" ").first } # ["b ", "c d"] => ["b", "c"]
+            .map { |name| name.gsub(/[^0-9a-z\-_]/i, '') }
+
+          puts "Done. Got #{mentions}"
+          puts "Converting usernames to engineers..."
+
+          mentioned_engineers = mentions.map { |username| find_engineer(github: username) }
+
+          puts "Done. Got #{mentioned_engineers}"
+
+          # add them to the list of usernames to ping
+          engineers_to_ping = engineers_to_ping.concat(mentioned_engineers).uniq.compact
+        end
+
+        puts "New list of engineers to ping: #{engineers_to_ping}."
+        puts "Starting pinging process for each engineer..."
+        engineers_to_ping.each do |engineer|
+          puts "looking at #{engineer}'s preferences..'"
+          next if engineer[:github_preferences][:frequency] == "off"
+
+          case engineer[:github_preferences][:ping_location]
+          when "dm", nil
+            puts "Preference was either 'dm' or nil, so sending DM."
+            private_message  = "New PR comment from <@#{lita_commenter.id}|#{commenter[:usernames][:slack]}>:\n"
+            private_message += "#{comment_url}\n#{comment}"
+            send_dm(engineer[:usernames][:slack], private_message)
+          when "eng-pr", "eng_pr"
+            puts "Preference was either 'eng-pr' or 'eng_pr', so alerting #eng-pr."
+            public_message  = "@#{engineer[:usernames][:slack]}, new PR mention: "
+            public_message += "#{comment_url}\n#{comment}"
+            alert_eng_pr(public_message)
+          end
+        end
+
+        puts "GitHub Hook successfully processed."
+
+        response
+      end
+
       def get_next_round_robin_reviewer
         engineers_with_rr_enabled = config.engineers.values.select { |eng| eng[:enable_round_robin] }
         next_reviewer = redis.get(RR_REDIS_KEY)
@@ -105,6 +393,20 @@ module Lita
         end
 
         next_reviewer
+      end
+
+      def update_next_round_robin_reviewer
+        engineers_with_rr_enabled = config.engineers.values.select { |eng| eng[:enable_round_robin] }
+        current_reviewer = get_next_round_robin_reviewer
+
+        current_reviewer_index = engineers_with_rr_enabled.find_index do |eng|
+          eng[:usernames][:slack] == current_reviewer
+        end
+
+        next_reviewer_index = (current_reviewer_index + 1) % engineers_with_rr_enabled.length
+        next_reviewer = engineers_with_rr_enabled[next_reviewer_index][:usernames][:slack]
+
+        redis.set(RR_REDIS_KEY, next_reviewer)
       end
 
       def alert_eng_pr(message)
--- spec/lita/handlers/github_pinger_spec.rb
+++ spec/lita/handlers/github_pinger_spec.rb
@@ -22,4 +22,47 @@ describe Lita::Handlers::GithubPinger, lita_handler: true do
     response = http.post("/ghping", '{"hello": "world", "events": ["pull_request_review_comment"]}')
     expect(response.body).to_not be_nil
   end
+
+  describe 'act on assign' do
+    before do
+      Lita::Room.create_or_update(1, name: 'eng-pr')
+      Lita::User.create(1, name: 'taylorlapeyre')
+    end
+
+    context 'pull request' do
+      it 'sends direct message' do
+        fake_json_request = {
+          action: 'assigned',
+          pull_request: {
+            assignee: {
+              login: 'taylorlapeyre'
+            }
+          }
+        }.to_json
+
+        response = http.post('/ghping', fake_json_request)
+
+        expect(replies.first).to match("You've been assigned to review a pull request")
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'issue' do
+      it 'sends direct message' do
+        fake_json_request = {
+          action: 'assigned',
+          issue: {
+            assignee: {
+                login: 'taylorlapeyre'
+            }
+          }
+        }.to_json
+
+        response = http.post('/ghping', fake_json_request)
+
+        expect(replies.first).to match("You've been assigned to review a issue")
+        expect(response.status).to eq(200)
+      end
+    end
+  end
 end
